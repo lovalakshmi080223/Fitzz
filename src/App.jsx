31ec7a480ff3c3acd94ec1702a0ce7dd995a5fe5
Lovalakshmi <lovalakshmianasuri@gmail.com>
2025-02-15 20:42:16 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from './components/Home';
-import SignUp from './components/SignUp'
-import Login from './components/Login'
-import Fitzzz from './components/Fitzzz'
-import ProtectedRoute from './ProtectedRoute';
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Lazy load components
+const Home = lazy(() => import("./components/Home"));
+const SignUp = lazy(() => import("./components/SignUp"));
+const Login = lazy(() => import("./components/Login"));
+const Fitzzz = lazy(() => import("./components/Fitzzz"));
+const ProtectedRoute = lazy(() => import("./ProtectedRoute"));
 
-
-
-
-function App () {
-  return(
-    <>
+function App() {
+  return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path= '/signUp' element = {<SignUp/>}/>
-        <Route path='/login' element ={<Login />} />
-        <Route path='/fitzzz' element ={<ProtectedRoute>
-          <Fitzzz />
-        </ProtectedRoute>} />
-        
-
-
-
-      </Routes>
-    
-    
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/fitzzz"
+            element={
+              <ProtectedRoute>
+                <Fitzzz />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-    
-    </>
   );
 }
-export default App; 
+
+export default App;
